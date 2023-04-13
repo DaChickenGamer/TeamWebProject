66a15b1a0739c1f8bf70acf1ce4e08c38ec22509
ChickenGamer <rjmichalski2006@gmail.com>
2023-04-13 10:33:01 -0400

    Homepage Has Content
    Made it so the homepage now contains content though it still has four rows.

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,11 @@
 const techNews = document.getElementById("technology");
 const minecraftBtn = document.getElementById("minecraft");
 const gamingBtn = document.getElementById("gaming");
+const generalBtn = document.getElementById("news");
 const newsType = document.getElementById("newsType");
 const newsQuery = document.getElementById("newsQuery");
 const newsdetails = document.getElementById("newsdetails");
-var currentNewsType = document.getElementById("gaming");
+var currentNewsType = document.getElementById("news");
 
 currentNewsType.classList.toggle("active");
 
@@ -17,35 +18,52 @@ const API_KEY = "";
 const TECHNOLOGY_NEWS = "https://newsapi.org/v2/top-headlines?country=us&category=technology&sortBy=publishedAt&pageSize=16&apiKey=";
 const MINECRAFT_NEWS = "https://newsapi.org/v2/everything?q=minecraft&language=en&pageSize=16&sortBy=publishedAt&apiKey="
 const GAMING_NEWS = "https://newsapi.org/v2/everything?language=en&q=gaming&pageSize=16&sortBy=publishedAt&apiKey="
+const GENERAL_NEWS = "https://newsapi.org/v2/everything?language=en&q=gaming&pageSize=16&sortBy=publishedAt&apiKey="
+
 
 window.onload = function() {
+    if (window.location.pathname === "/articles.html"){
     newsType.innerHTML="<h4>Gaming</h4>";
     fetchGamingNews();
+
+    techNews.addEventListener("click",function(){
+        newsType.innerHTML="<h4>Technology</h4>";
+        fetchTechNews();
+        currentNewsType.classList.toggle("active");
+        currentNewsType = techNews;
+        currentNewsType.classList.toggle("active");
+    });
+    
+    gamingBtn.addEventListener("click",function(){
+        newsType.innerHTML="<h4>Gaming</h4>";
+        fetchGamingNews();
+        currentNewsType.classList.toggle("active");
+        currentNewsType = gamingBtn;
+        currentNewsType.classList.toggle("active");
+    });
+    
+    minecraftBtn.addEventListener("click",function(){
+        newsType.innerHTML="<h4>Minecraft</h4>";
+        fetchMinecraftNews();
+        currentNewsType.classList.toggle("active");
+        currentNewsType = minecraftBtn;
+        currentNewsType.classList.toggle("active");
+    });
+    }
+    else if (window.location.pathname === "/index.html"){
+        newsType.innerHTML="<h4>Home<h4">
+        fetchGeneralNews();
+
+        generalBtn.addEventListener("click",function(){
+            newsType.innerHTML="<h4>HOME</h4>";
+            fetchGeneralNews();
+            currentNewsType.classList.toggle("active");
+            currentNewsType = generalBtn;
+            currentNewsType.classList.toggle("active");
+        });
+    }
 };
 
-techNews.addEventListener("click",function(){
-    newsType.innerHTML="<h4>Technology</h4>";
-    fetchTechNews();
-    currentNewsType.classList.toggle("active");
-    currentNewsType = techNews;
-    currentNewsType.classList.toggle("active");
-});
-
-gamingBtn.addEventListener("click",function(){
-    newsType.innerHTML="<h4>Gaming</h4>";
-    fetchGamingNews();
-    currentNewsType.classList.toggle("active");
-    currentNewsType = gamingBtn;
-    currentNewsType.classList.toggle("active");
-});
-
-minecraftBtn.addEventListener("click",function(){
-    newsType.innerHTML="<h4>Minecraft</h4>";
-    fetchMinecraftNews();
-    currentNewsType.classList.toggle("active");
-    currentNewsType = minecraftBtn;
-    currentNewsType.classList.toggle("active");
-});
 
 const fetchTechNews = async () => {
     const response = await fetch(TECHNOLOGY_NEWS+API_KEY);
@@ -82,6 +100,21 @@ const fetchGamingNews = async () => {
 
 const fetchMinecraftNews = async () => {
     const response = await fetch(MINECRAFT_NEWS+API_KEY);
+    newsDataArr = [];
+    if(response.status >=200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        // handle errors
+        console.log(response.status, response.statusText);
+        newsdetails.innerHTML = "<h5>No data found.</h5>"
+        return;
+    }
+
+    displayNews();
+}
+const fetchGeneralNews = async () => {
+    const response = await fetch(GENERAL_NEWS+API_KEY);
     newsDataArr = [];
     if(response.status >=200 && response.status < 300) {
         const myJson = await response.json();
